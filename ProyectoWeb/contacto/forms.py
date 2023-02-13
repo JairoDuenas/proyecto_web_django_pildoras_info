aa3a9bf149c3c3ae600b51ef875b1dcac62c66cb
JairoDuenas <jairoduenas.ing@gmail.com>
2023-02-13 17:01:59 -0500

    Rescatando información de formulario contacto App[contacto]

--- ProyectoWeb/contacto/forms.py
+++ ProyectoWeb/contacto/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 class FormularioContacto(forms.Form):
   nombre = forms.CharField(label='Nombre', required=True)
-  email = forms.CharField(label='Email', max_length=100)
+  email = forms.EmailField(label='Email', max_length=100)
   contenido = forms.CharField(label='Contenido', widget=forms.Textarea(attrs={'cols': 20, 'rows': 5}))
