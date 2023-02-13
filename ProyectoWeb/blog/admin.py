0a8279acfe196fcc9c928b1c6fe6894dbfc5b17a
JairoDuenas <jairoduenas.ing@gmail.com>
2023-02-13 08:12:08 -0500

    Registrando app[blog] al panel de administración

--- ProyectoWeb/blog/admin.py
+++ ProyectoWeb/blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Categoria, Post
 
 # Register your models here.
+
+class Categoria_add_admin(admin.ModelAdmin):
+  readonly_fields=('created', 'updated')
+
+class Post_add_admin(admin.ModelAdmin):
+  readonly_fields=('created', 'updated')
+
+admin.site.register(Categoria, Categoria_add_admin)
+admin.site.register(Post, Post_add_admin)
