0eddb6bb6c0c3285a9ad5029ba03559997cec7bd
JairoDuenas <jairoduenas.ing@gmail.com>
2023-02-15 10:05:49 -0500

    Registrando nuevos usuarios

--- ProyectoWeb/autenticacion/views.py
+++ ProyectoWeb/autenticacion/views.py
@@ -1,16 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib import messages
 
 # Create your views here.
 
 class VRegistro(View):
   def get(self, request):
-    form=UserCreationForm()
-    return render(request, "registro/registro.html", {"form":form})
+      form = UserCreationForm()
+      return render(request, "registro/registro.html", {"form": form})
 
   def post(self, request):
-    pass
+      form = UserCreationForm(request.POST)
 
-""" def autenticacion(request):
-  return render(request, "registro/registro.html", {}) """
+      if form.is_valid():
+        usuario = form.save()
+        login(request, usuario)
+        return redirect('Home')
+      else:
+        for msg in form.error_messages:
+          messages.error(request, form.error_messages[msg])
+        return render(request, "registro/registro.html", {"form": form})
